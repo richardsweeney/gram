074625b99779fe963b801f589acf229efff97dbd
Richard Sweeney <theorobman@gmail.com>
2016-07-03 12:49:17 +0200

    Initial commit

---
+++ local-config.php
@@ -0,0 +1,14 @@
+<?php
+
+define( 'DB_NAME', 'gram' );
+define( 'DB_USER', 'root' );
+define( 'DB_PASSWORD', 'root' );
+define( 'DB_HOST', 'localhost' );
+
+// This is a good local dev setup.
+define( 'SAVEQUERIES', true );
+define( 'WP_DEBUG', true );
+define( 'WP_DEBUG_LOG', true );
+define( 'WP_DEBUG_DISPLAY', false );
+
+define( 'WPLANG', 'sv_SE' );
