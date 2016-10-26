8059dd6eb3d86dc4f2bc2af7af9ec777b4ce8384
Richard Sweeney <theorobman@gmail.com>
2016-10-26 21:31:53 +0200

    cap config

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,5 +4,5 @@ role :app, %w{root@46.101.223.151}
 
 set :deploy_to, '/usr/share/nginx/gram'
 set :linked_dirs, %w{content/uploads }
-set :linked_files, %w{local-config.php}
+set :linked_files, %w{local-config.php robots.txt}
 set :branch, "master"
