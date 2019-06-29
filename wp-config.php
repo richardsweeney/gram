2adfbcf20aa06ebbef119904379eaeeca27a21f2
Richard Sweeney <richard.sweeney@angrycreative.se>
2019-06-29 16:38:29 +0200

    Updates, fixes

--- wp-config.php
+++ wp-config.php
@@ -7,7 +7,9 @@
 require 'local-config.php';
 
 define( 'WP_CONTENT_DIR', dirname( __FILE__ ) . '/content' );
-define( 'WP_CONTENT_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/content' );
+if ( ! empty( $_SERVER['HTTP_HOST'] ) ) {
+	define( 'WP_CONTENT_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/content' );
+}
 
 define( 'AUTOMATIC_UPDATER_DISABLED', true );
 define( 'WP_CACHE_KEY_SALT', md5( DB_NAME . __FILE__ ) );
@@ -18,9 +20,10 @@ define( 'FS_METHOD', 'direct' );
 define( 'DB_CHARSET', 'utf8mb4' );
 define( 'DB_COLLATE', 'utf8mb4_swedish_ci' );
 
-$table_prefix  = 'wp_';
+$table_prefix = 'wp_';
 
-if ( ! defined( 'ABSPATH' ) )
-    define( 'ABSPATH', dirname( __FILE__ ) . '/wp/' );
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', dirname( __FILE__ ) . '/wp/' );
+}
 
 require ABSPATH . 'wp-settings.php';
