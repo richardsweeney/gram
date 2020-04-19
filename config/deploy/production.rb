99a1a0355bf1e59500cfa5506e77663600e21b32
Richard Sweeney <theorboman@gmail.com>
2020-04-19 17:11:03 +0200

    Update theme

--- config/deploy/production.rb
+++
@@ -1,8 +0,0 @@
-set :stage, :production
-
-role :app, %w{root@46.101.223.151}
-
-set :deploy_to, '/usr/share/nginx/gram'
-set :linked_dirs, %w{content/uploads }
-set :linked_files, %w{local-config.php robots.txt}
-set :branch, "master"
--- config/deploy/stage.rb
+++
@@ -1,8 +0,0 @@
-set :stage, :stage
-
-role :app, %w{root@46.101.223.151}
-
-set :deploy_to, '/usr/share/nginx/gram-stage'
-set :linked_dirs, %w{content/uploads }
-set :linked_files, %w{local-config.php robots.txt}
-set :branch, "master"
